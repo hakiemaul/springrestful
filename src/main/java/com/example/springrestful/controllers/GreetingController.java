f7042eaa74d62885543cf6252455b0298f1f55b2
Aulia Hakiem <noersedya@gmail.com>
2020-11-22 11:02:00 +0700

    init commit with working controller

---
+++ src/main/java/com/example/springrestful/controllers/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.springrestful.controllers;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.example.springrestful.models.Greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template = "%s, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+    public Greeting greeting(
+        @RequestParam(value = "name", defaultValue = "World") String name,
+        @RequestParam(value = "greeting", defaultValue = "Hello") String greeting
+    ) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, greeting, name));
+	}
+}
